07d267c3d626374eadd815fc4457ea773e08e335
ozum <ozum@ozum.net>
2018-03-15 11:27:46 +0300

    Test

--- src/scripts/commit.js
+++ src/scripts/commit.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { bootstrap } = require('commitizen/dist/cli/git-cz');
 
-console.log('PATH: ' + path.join(__dirname, '../../node_modules/commitizen');
+console.log('PATH: ' + path.join(__dirname, '../../node_modules/commitizen'));
 
 bootstrap({
   cliPath: path.join(__dirname, '../../node_modules/commitizen'),
