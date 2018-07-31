4c0a26699b6f2bbbcf3c237aeec28de1ed06540d
ozum <ozum@ozum.net>
2018-07-31 12:57:59 +0300

    fix: rename babel-macros in .babelrc to babel-plugin-macros

--- config.js
+++ config.js
@@ -3,5 +3,4 @@ const path = require("path");
 
 const config = fs.existsSync(path.join(__dirname, "src")) ? require("./src/config") : require("./lib/config");
 
-console.log(config);
 module.exports = config;
