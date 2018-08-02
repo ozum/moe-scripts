c049f6908af6a42c0babe8a8cbb9faff779bba9a
ozum <ozum@ozum.net>
2018-08-02 14:03:30 +0300

    fix: fix jext coverage for non-compiled projects

--- src/config/jest.config.js
+++ src/config/jest.config.js
@@ -3,6 +3,7 @@ const project = require("../project");
 
 const here = p => path.join(__dirname, p);
 
+const dir = project.isCompiled ? "src" : "lib";
 const useBuiltInBabelConfig = !project.hasFileSync(".babelrc") && !project.package.has("babel");
 
 const ignores = ["/node_modules/", "/fixtures/", "/__tests__/helpers/", "/__test_supplements__/", "/__test_helpers__/", "__mocks__"];
@@ -11,10 +12,10 @@ const jestConfig = {
   roots: [project.hasFileSync("src", project.fromRoot("src"), project.fromRoot("lib"))],
   testEnvironment: project.hasAnyDep(["webpack", "rollup", "react"], "jsdom", "node"),
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  collectCoverageFrom: ["src/**/*.+(js|jsx|ts|tsx)"],
+  collectCoverageFrom: [`${dir}/**/*.+(js|jsx|ts|tsx)`],
   testMatch: ["**/__tests__/**/*.+(js|jsx|ts|tsx)", "**/*.(test|spec).(js|jsx|ts|tsx)"],
   testPathIgnorePatterns: [...ignores],
-  coveragePathIgnorePatterns: [...ignores, "src/(umd|cjs|esm)-entry.js$", "/src/bin/"], // Exclude CLI files located in bin, because cli cannot be instrumented by JEST as of 22
+  coveragePathIgnorePatterns: [...ignores, `${dir}/(umd|cjs|esm)-entry.js$`, `/${dir}/bin/`], // Exclude CLI files located in bin, because cli cannot be instrumented by JEST as of 22
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
   watchPathIgnorePatterns: [...ignores],
   coverageThreshold: {
